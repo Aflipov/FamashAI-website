52debc024697864428d87399ef2e5abd0cb6a7a3
aflipov <a.filippov04@yandex.ru>
2023-04-13 19:37:00 +0300

    3d face model added

--- js/3d_preview.js
+++ js/3d_preview.js
@@ -11,9 +11,9 @@ function main() {
 	const fov = 45;
 	const aspect = 2;	// the canvas default
 	const near = 0.1;
-	const far = 100;
+	const far = 10000;
 	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-	camera.position.set(0, 10, 20);
+	camera.position.set(0, 30, 460);
 
 	const controls = new OrbitControls(camera, canvas);
 	controls.target.set(0, 5, 0);
@@ -22,67 +22,77 @@ function main() {
 	const scene = new THREE.Scene();
 	// scene.background = new THREE.Color('black');
 
+	// {
+	// 	const planeSize = 40;
+
+	// 	const loader = new THREE.TextureLoader();
+	// 	const texture = loader.load('https://threejs.org/manual/examples/resources/images/checker.png');
+	// 	texture.encoding = THREE.sRGBEncoding;
+	// 	texture.wrapS = THREE.RepeatWrapping;
+	// 	texture.wrapT = THREE.RepeatWrapping;
+	// 	texture.magFilter = THREE.NearestFilter;
+	// 	const repeats = planeSize / 2;
+	// 	texture.repeat.set(repeats, repeats);
+
+	// 	const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+	// 	const planeMat = new THREE.MeshPhongMaterial({
+	// 		map: texture,
+	// 		side: THREE.DoubleSide,
+	// 	});
+	// 	const mesh = new THREE.Mesh(planeGeo, planeMat);
+	// 	mesh.rotation.x = Math.PI * -.5;
+	// 	scene.add(mesh);
+	// }
+
 	{
-		const planeSize = 40;
-
-		const loader = new THREE.TextureLoader();
-		const texture = loader.load('https://threejs.org/manual/examples/resources/images/checker.png');
-		texture.encoding = THREE.sRGBEncoding;
-		texture.wrapS = THREE.RepeatWrapping;
-		texture.wrapT = THREE.RepeatWrapping;
-		texture.magFilter = THREE.NearestFilter;
-		const repeats = planeSize / 2;
-		texture.repeat.set(repeats, repeats);
-
-		const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-		const planeMat = new THREE.MeshPhongMaterial({
-			map: texture,
-			side: THREE.DoubleSide,
-		});
-		const mesh = new THREE.Mesh(planeGeo, planeMat);
-		mesh.rotation.x = Math.PI * -.5;
-		scene.add(mesh);
-	}
-
-	{
-		const skyColor = 0xB1E1FF;	// light blue
-		const groundColor = 0xB97A20;	// brownish orange
-		const intensity = 1;
-		const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+		const color = 0xFFFFFF;
+		const intensity = 0.15;
+		const light = new THREE.AmbientLight(color, intensity);
 		scene.add(light);
 	}
 
 	{
 		const color = 0xFFFFFF;
-		const intensity = 1;
+		const intensity = 0.4;
 		const light = new THREE.DirectionalLight(color, intensity);
-		// light.position.set(5, 10, 2);
-		light.position.set(0, 10, 20);
+		light.position.set(0, 200, 460);
 		scene.add(light);
-		scene.add(light.target);
-	}
-
-	{
-		const mtlLoader = new MTLLoader();
-		mtlLoader.load('https://threejs.org/manual/examples/resources/models/windmill/windmill.mtl', (mtl) => {
-			mtl.preload();
-			const objLoader = new OBJLoader();
-			objLoader.setMaterials(mtl);
-			objLoader.load('https://threejs.org/manual/examples/resources/models/windmill/windmill.obj', (root) => {
-			// objLoader.load('obj models/windmill/windmill.obj', (root) => {
-				scene.add(root);
-			});
-		});
+		// scene.add(light.target);
 	}
 
 	// {
-	// 	const objLoader = new OBJLoader();
-	// 	// objLoader.load('https://threejs.org/manual/examples/resources/models/windmill/windmill.obj', (root) => {
-	// 	// objLoader.load('obj models/windmill/windmill.obj', (root) => {
-	// 	objLoader.load('obj models/6-45/6-45-in_mesh.obj', (root) => {
-	// 		scene.add(root);
+	// 	const mtlLoader = new MTLLoader();
+	// 	mtlLoader.load('https://threejs.org/manual/examples/resources/models/windmill/windmill.mtl', (mtl) => {
+	// 		mtl.preload();
+	// 		const objLoader = new OBJLoader();
+	// 		objLoader.setMaterials(mtl);
+	// 		objLoader.load('https://threejs.org/manual/examples/resources/models/windmill/windmill.obj', (root) => {
+	// 			scene.add(root);
+	// 		});
 	// 	});
 	// }
+
+	// {
+	// 	const objLoader = new OBJLoader();
+	// 	objLoader.load('obj models/5-36/5-36-in_mesh.obj', (root) => {
+	// 		scene.add(root);
+	// 		root.position.set(-200, -200, -120)
+	// 	});
+	// }
+
+	{
+		const material = new THREE.MeshPhongMaterial({
+			color: 0x000000,
+			flatShading: true,
+			side: THREE.DoubleSide
+		});
+		const objLoader = new OBJLoader();
+		// objLoader.setMaterials(material)
+		objLoader.load('obj models/5-36/5-36-in_mesh.obj', (root) => {
+			scene.add(root);
+			root.position.set(-200, -200, -120)
+		});
+	}
 
 	function resizeRendererToDisplaySize(renderer) {
 		const canvas = renderer.domElement;
